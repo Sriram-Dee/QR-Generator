34ee17d6116f29895fccff6ba4a6560fd4d6e5f4
Sriram-Dee <sriram14jeeva12@gmail.com>
2024-01-31 23:43:38 +0530

    All files are set

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,50 @@
+import { useState } from 'react'
 import './App.css'
 
+
 function App() {
+
+  const [img, setImg] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [qrData, setQrData] = useState("https://sriram-dee.github.io/QR-Generator/")
+  const [size, setSize] = useState('200');
+
+    async function generateQR(){
+      setLoading(true);
+      try{
+        const url = `https://api.qrserver.com/v1/create-qr-code/?size=${size}x${size}&data=${encodeURIComponent(qrData)}`;
+        setImg(url)
+      }catch(error){
+        console.log("Error on generating QR ", error);
+      }finally{
+        setLoading(false);
+      }
+    }
+
+    function downloadQr(){
+      fetch(img).then((reponse)=>reponse.blob()).then((blob)=>{
+        const link = document.createElement("a");
+        link.href= URL.createObjectURL(blob);
+        link.download="Your_QR.png";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      });
+    }
 
   return (
     <>
       <h2>QR CODE GENERATOR</h2>
-      <img src="" alt="QR-Code" />
+      {img && <img src={img} alt="QR-Code" />}
+      {loading && <p>Please wait...</p>}
       <div className='container'>
         <label htmlFor="data">Data for your QR code</label>
-        <input type="text" id="data" value={""}/>
+        <input type="text" id="data" onChange={(e)=>setQrData(e.target.value)} placeholder='Enter your text or URL'/>
         <label htmlFor="size">Image size (e.g, 200);</label>
-        <input type="text" id='size' value={"200"}/>
+        <input type="text" id='size' onChange={(e)=>setSize(e.target.value)} placeholder='E.g: 150'/>
         <div className="btns">
-          <button className="generate">Generate QR</button>
-          <button className="download">Download QR</button>
+          <button className="generate"  onClick={generateQR}>Generate QR</button>
+          <button className="download" onClick={downloadQr}>Download QR</button>
         </div>
       </div>
     </>
